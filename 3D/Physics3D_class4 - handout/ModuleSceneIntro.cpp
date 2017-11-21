00fbfb05257a6dceaaeb09382eff6b9f24c92828
aleixgab <aleix.gab@gmail.com>
2017-11-21 11:43:32 +0100

    4th 3d class

---
+++ 3D/Physics3D_class4 - handout/ModuleSceneIntro.cpp
@@ -0,0 +1,79 @@
+#include "Globals.h"
+#include "Application.h"
+#include "ModuleSceneIntro.h"
+#include "Primitive.h"
+#include "PhysBody3D.h"
+
+ModuleSceneIntro::ModuleSceneIntro(Application* app, bool start_enabled) : Module(app, start_enabled)
+{
+}
+
+ModuleSceneIntro::~ModuleSceneIntro()
+{}
+
+// Load assets
+bool ModuleSceneIntro::Start()
+{
+	LOG("Loading Intro assets");
+	bool ret = true;
+
+	App->camera->Move(vec3(1.0f, 1.0f, 0.0f));
+	App->camera->LookAt(vec3(0, 0, 0));
+
+	// TODO 2: Chain few N spheres together to form a snake
+
+	PhysBody3D* bodyB = nullptr;
+	for (int i = 0; i < 10; i++) {
+
+		Sphere sphere;
+		sphere.SetPos(i * 2, 1, 0);
+		PhysBody3D* bodyA = App->physics->AddBody(sphere);
+		if (bodyB != nullptr)
+			App->physics->AddConstraint(*bodyA, *bodyB, { -1,0,0 }, { 1,0,0 });
+		bodyB = bodyA;
+	}
+
+
+
+	// TODO 4: Chain few N spheres together to form a a bike's sphere
+	// Be sure to put the right axis
+
+
+	return ret;
+}
+
+// Load assets
+bool ModuleSceneIntro::CleanUp()
+{
+	LOG("Unloading Intro scene");
+
+	return true;
+}
+
+// Update
+update_status ModuleSceneIntro::Update(float dt)
+{
+	Plane p(0, 1, 0, 0);
+	p.axis = true;
+	p.Render();
+
+	/* Uncomment when ready
+	for(int i = 0; i < MAX_SNAKE; ++i)
+	{
+		pb_snake[i]->GetTransform(&(s_snake[i].transform));
+		s_snake[i].Render();
+	}
+
+	for(int i = 0; i < MAX_SNAKE; ++i)
+	{
+		pb_snake2[i]->GetTransform(&(s_snake2[i].transform));
+		s_snake2[i].Render();
+	}*/
+
+	return UPDATE_CONTINUE;
+}
+
+void ModuleSceneIntro::OnCollision(PhysBody3D* body1, PhysBody3D* body2)
+{
+}
+
